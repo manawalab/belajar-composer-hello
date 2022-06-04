b03893dcb8780736bdc99136b7a9e599195ef1f2
manawalab <3ngkaumar@gmail.com>
2022-06-05 02:35:29 +0800

    Contoh Library

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Uproject\Library;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+}
